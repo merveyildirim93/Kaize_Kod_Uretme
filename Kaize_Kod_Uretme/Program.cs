2846b89d2b029482b36ea5f2028170f08ea4ed3e
Merve Yıldırım <merve.yildirimm.93@hotmail.com>
2024-04-19 02:24:38 +0300

    kodların kontrol edilmesi için düzenlemeler yapıldı

--- Kaize_Kod_Uretme/Program.cs
+++ Kaize_Kod_Uretme/Program.cs
@@ -1,13 +1,23 @@
-﻿using System.Numerics;
+﻿using System.Diagnostics;
+using System.Numerics;
+using System.Text;
+using static System.Net.Mime.MediaTypeNames;
 
 class Program
 {
     static void Main()
     {
-        List<string> generatedCodes = GenerateUniqueCodes(100);
-        foreach (var code in generatedCodes)
-        {
-            Console.WriteLine(code);
+        List<string> generatedCodes = GenerateUniqueCodes(1000);
+        foreach (var code in generatedCodes) {
+            var control = IsValidCode(code);
+            if (control)
+            {
+                Console.WriteLine(code + "--- KOD GEÇERLİ");
+            }
+            else
+            {
+                Console.WriteLine(code + "--- KOD GEÇERLİ DEĞİL");
+            }
         }
     }
     static List<string> GenerateUniqueCodes(int numCodes)
@@ -23,8 +33,8 @@ class Program
             indices.Add(random.Next(1, 23)); // 1 ve 23 arasında rastgele sayı üretir
         }
 
-        // 7'li permütasyonu al ve numCodes sayısına kadar olan olasılıkları elde et
-        List<List<int>> permutations = GetPermutations(indices, 7).Distinct().Take(numCodes).ToList();
+        // 8'li permütasyonu al ve numCodes sayısına kadar olan olasılıkları elde et
+        List<List<int>> permutations = GetPermutations(indices, 8).Distinct().Take(numCodes).ToList();
 
         foreach (List<int> permutation in permutations)
         {
@@ -41,20 +51,22 @@ class Program
 
     static string GenerateCode(List<int> permutation, Random random)
     {
+        string charSet = "ACDEFGHKLMNPRTXYZ234579";
         string code = "";
         int start = random.Next(1, 23); // Kodun başlangıç indexi için random bir değer seçiyoruz
         int prevIndex = start;
 
-        //BU KISMI KONTROL İÇİN DE KULLAN
         foreach (int index in permutation)
         {
             int nextIndex = (prevIndex + index - 1) % 23; // Modulus 23 işlemi
-            char nextChar = "ACDEFGHKLMNPRTXYZ234579"[nextIndex];
+            char nextChar = charSet[nextIndex];
             code += nextChar;
             prevIndex = nextIndex;
         }
+
         return code;
     }
+
 
     static List<List<T>> GetPermutations<T>(List<T> list, int length)
     {
@@ -66,26 +78,20 @@ class Program
 
     static bool IsValidCode(string code)
     {
-        int[] connections = { 2, 5, 6, 10, 16, 18, 21, 22 }; // Bağlantılar
+        string charSet = "ACDEFGHKLMNPRTXYZ234579";
+        StringBuilder indeksString = new StringBuilder();
         BigInteger codeNum = 0;
 
-        // Karakterlerin indekslerini al ve bağlantıları hesapla
-        List<int> indexes = code.Select(c => "ACDEFGHKLMNPRTXYZ234579".IndexOf(c) + 1).ToList();
-        List<int> differences = new List<int>();
-        for (int i = 1; i < indexes.Count; i++)
+        foreach (char karakter in code)
         {
-            int diff = (indexes[i] - indexes[i - 1] + 23) % 23;
-            differences.Add(diff);
+            int indeks = charSet.IndexOf(karakter);
+            indeksString.Append(indeks);
         }
 
-        // Bağlantıları string olarak birleştir ve BigInteger'a dönüştür
-        string connectionString = string.Join("", differences);
-        bool isValidBigInt = BigInteger.TryParse(connectionString, out codeNum);
-        if (!isValidBigInt) return false;
-
-        // 1. ve 10.000.000. satırlar arasında veya eşit olmalı
-        BigInteger minNum = BigInteger.Parse("10161852622");
-        BigInteger maxNum = BigInteger.Parse("100951416107142359695918155112222518313");
+        string indeksler = indeksString.ToString();
+        BigInteger.TryParse(indeksler, out codeNum);
+        BigInteger minNum = BigInteger.Parse("161799556");
+        BigInteger maxNum = BigInteger.Parse("9223372036854775807"); //int64ün max değeri
         return codeNum >= minNum && codeNum <= maxNum;
     }
 }
