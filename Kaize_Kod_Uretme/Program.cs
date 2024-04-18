38885dad66a364bb0d22a7762c5ac9f8e297914c
Merve Yıldırım <merve.yildirimm.93@hotmail.com>
2024-04-18 10:51:19 +0300

    açıklama satırları kaldırıldı

--- Kaize_Kod_Uretme/Program.cs
+++ Kaize_Kod_Uretme/Program.cs
@@ -1,99 +1,15 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Numerics;
-
-//class Program
-//{
-//    static void Main()
-//    {
-//        List<string> codes = GenerateCodes(100);
-//        foreach (string code in codes)
-//        {
-//            Console.WriteLine(code);
-//        }
-//    }
-//    static List<string> GenerateCodes(int numCodes)
-//    {
-//        List<string> generatedCodes = new List<string>();
-//        Random random = new Random();
-
-//        // 2 ile 22 arasındaki sayıların kümesini oluştur
-//        List<int> indices = Enumerable.Range(2, 8).ToList();
-
-//        // 7'li permütasyonu al ve numCodes sayısına kadar olan olasılıkları elde et
-//        List<List<int>> permutations = GetPermutations(indices, 7);
-//        permutations = permutations.Take(numCodes).ToList();
-
-//        foreach (List<int> permutation in permutations)
-//        {
-//            string code = "";
-//            int start = random.Next(1, 23); // Kodun başlangıç indexi için random bir değer atıyoruz
-//            int prevIndex = start;
-//            foreach (int index in permutation)
-//            {
-//                int nextIndex = (prevIndex + index - 1) % 23; // Modulus 23 işlemi
-//                char nextChar = "ACDEFGHKLMNPRTXYZ234579"[nextIndex];
-//                code += nextChar;
-//                prevIndex = nextIndex;
-//            }
-//            generatedCodes.Add(code);
-//        }
-
-//        return generatedCodes;
-//    }
-
-//    static List<List<T>> GetPermutations<T>(List<T> list, int length)
-//    {
-//        if (length == 1) return list.Select(t => new List<T> { t }).ToList();
-//        return GetPermutations(list, length - 1)
-//            .SelectMany(t => list.Where(e => !t.Contains(e)),
-//                        (t1, t2) => t1.Concat(new List<T> { t2 }).ToList()).ToList();
-//    }
-
-//    static bool IsValidCode(string code)
-//    {
-//        int[] connections = { 2, 5, 6, 10, 16, 18, 21, 22 }; // Bağlantılar
-//        BigInteger codeNum = 0;
-
-//        // Karakterlerin indekslerini al ve bağlantıları hesapla
-//        List<int> indexes = code.Select(c => "ACDEFGHKLMNPRTXYZ234579".IndexOf(c) + 1).ToList();
-//        List<int> differences = new List<int>();
-//        for (int i = 1; i < indexes.Count; i++)
-//        {
-//            int diff = (indexes[i] - indexes[i - 1] + 23) % 23;
-//            differences.Add(diff);
-//        }
-
-//        // Bağlantıları string olarak birleştir ve BigInteger'a dönüştür
-//        string connectionString = string.Join("", differences);
-//        bool isValidBigInt = BigInteger.TryParse(connectionString, out codeNum);
-//        if (!isValidBigInt) return false;
-
-//        // 1. ve 10.000.000. satırlar arasında veya eşit olmalı
-//        BigInteger minNum = BigInteger.Parse("10161852622");
-//        BigInteger maxNum = BigInteger.Parse("100951416107142359695918155112222518313");
-//        return codeNum >= minNum && codeNum <= maxNum;
-//    }
-//}
-
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Numerics;
+﻿using System.Numerics;
 
 class Program
 {
     static void Main()
     {
         List<string> generatedCodes = GenerateUniqueCodes(100);
-
         foreach (var code in generatedCodes)
         {
             Console.WriteLine(code);
         }
     }
-
     static List<string> GenerateUniqueCodes(int numCodes)
     {
         List<string> generatedCodes = new List<string>();
@@ -146,5 +62,30 @@ class Program
         return GetPermutations(list, length - 1)
             .SelectMany(t => list.Where(e => !t.Contains(e)),
                         (t1, t2) => t1.Concat(new List<T> { t2 }).ToList()).ToList();
+    }
+
+    static bool IsValidCode(string code)
+    {
+        int[] connections = { 2, 5, 6, 10, 16, 18, 21, 22 }; // Bağlantılar
+        BigInteger codeNum = 0;
+
+        // Karakterlerin indekslerini al ve bağlantıları hesapla
+        List<int> indexes = code.Select(c => "ACDEFGHKLMNPRTXYZ234579".IndexOf(c) + 1).ToList();
+        List<int> differences = new List<int>();
+        for (int i = 1; i < indexes.Count; i++)
+        {
+            int diff = (indexes[i] - indexes[i - 1] + 23) % 23;
+            differences.Add(diff);
+        }
+
+        // Bağlantıları string olarak birleştir ve BigInteger'a dönüştür
+        string connectionString = string.Join("", differences);
+        bool isValidBigInt = BigInteger.TryParse(connectionString, out codeNum);
+        if (!isValidBigInt) return false;
+
+        // 1. ve 10.000.000. satırlar arasında veya eşit olmalı
+        BigInteger minNum = BigInteger.Parse("10161852622");
+        BigInteger maxNum = BigInteger.Parse("100951416107142359695918155112222518313");
+        return codeNum >= minNum && codeNum <= maxNum;
     }
 }
